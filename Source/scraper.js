28428b5fde3897ceda7ae238ffbc6aa0a8919010
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-03-14 21:26:30 +0100

    Scraper can now grab all needed data, saving to csv is still required...

--- Source/scraper.js
+++ Source/scraper.js
@@ -25,25 +25,48 @@ function createDataFolderIfNeeded() {
 function grabShirtLinks(startUrl) {
     return scrapeIt(startUrl, {
         links: {
-            listItem : ".products li",
-            data : {
-                link: { 
+            listItem: ".products li",
+            data: {
+                link: {
                     selector: "a",
-                    attr : "href"
+                    attr: "href"
                 }
             }
         }
     });
 }
 
+function grabShirtData(url) {
+    return scrapeIt(url, {
+        price: { selector: ".price" },
+        title: { selector: "head title" },
+        imageUrl: { selector: ".shirt-picture img", attr: "src" }
+    });
+}
 
 // Requirement 1: Create that data folder if it does not exist...
 createDataFolderIfNeeded();
 
-
+// Requirement 4: Dynamically load all urls from this page that point
+//                to shirts!
 grabShirtLinks("http://shirts4mike.com/shirts.php")
-.then(({ data, response }) => {
-    console.log(data.links);
-});
+    .then(({ data, response }) => {
 
+        for (let i = 0; i < data.links.length; i++) {
+            let link = data.links[i].link;
 
+            console.log("grabbing " + link + " ...");
+
+            // Requirement 5 : Then grab data from the shirts...
+            grabShirtData("http://shirts4mike.com/" + link)
+                .then(({ data, response }) => {
+                    let shirt = {
+                        price: data.price,
+                        title: data.title,
+                        url: link,
+                        imageUrl: data.imageUrl
+                    }
+                    console.log(shirt);
+                });
+        }
+    });
